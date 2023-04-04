bc076c4eab1227d99157f6e9377fb3fe839199f3
Disuko <disukomusic@gmail.com>
2023-04-04 19:17:09 -0400

    started on inventory and weapon stuff

---
+++ Assets/Scripts/Interactables/Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Interactable 
+{ 
+    public int id; 
+    public string title; 
+    public string description; 
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+    
+    public Item(int id, string title, string description, Dictionary<String,int> stats, Sprite icon)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.stats = stats;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+
+    }
+}
---
+++ Assets/Scripts/Interactables/Weapon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Interactable
+{
+    private Inventory _inventory;
+    public override void Interact()
+    {
+    }
+}
