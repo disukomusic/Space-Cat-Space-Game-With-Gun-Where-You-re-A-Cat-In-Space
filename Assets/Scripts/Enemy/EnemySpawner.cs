a090f8d48482d14b665369a4c847f2fc45fa84a3
sunflowers33d <37161028+sunflowers33d@users.noreply.github.com>
2023-04-16 19:34:30 -0400

    init automatic enemy spawning
    10 enemies now spawn in on their own after waitSecondsTime has passed, and they will spawn every subWaitSeconds. checking for how many are spawned is yet to be implemented

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -9,23 +9,36 @@ public class EnemySpawner : MonoBehaviour
     // goal: be an object or objects on the floor that spawn enemies
     private EnemyPooler _objectPooler;
     public GameObject enemyPrefab;
+    [SerializeField] private float waitSecondsTime;
+    [SerializeField] private float subWaitSeconds;
 
     private void Awake()
     {
         _objectPooler = GetComponent<EnemyPooler>();
     }
 
+    private void Start()
+    {
+        StartCoroutine(SpawnAtTime());
+    }
+    
+
     public void SpawnEnemy()
     {
         
         Enemy enemy = _objectPooler.CreateGameObject(transform.position);
     }
-    private void Update()
+
+    public IEnumerator SpawnAtTime()
     {
-        // todo placeholder, should spawn on its own
-        if (Input.GetKeyDown(KeyCode.G))
+        // currently spawns enemies at the spawn point after waitSecondsTime has passed,
+        // and spawns 10 enemies every subWaitSeconds.
+        // todo check the count of the object pooler list to spawn in enemies if it is less than a certain number
+        yield return new WaitForSeconds(waitSecondsTime);
+        for (int i = 0; i < 10; i++)
         {
-           SpawnEnemy();
+            yield return new WaitForSeconds(subWaitSeconds);
+            SpawnEnemy();
         }
     }
 }
