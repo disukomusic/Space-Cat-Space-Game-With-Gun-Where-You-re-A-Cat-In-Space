43dcbd361868bf6416db5358117d248f6d8cfb1d
Disuko <disukomusic@gmail.com>
2023-04-19 22:21:40 -0400

    added penguino enemy

---
+++ Assets/Scripts/Enemy/Penguino.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+public class Penguino : MonoBehaviour
+{
+    [SerializeField] private GameObject icePrefab;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ThrowIce();
+        }
+    }
+
+    void ThrowIce()
+    {
+        GameObject ice = Instantiate(icePrefab, transform.position, Quaternion.identity);
+
+        Vector3 throwDirection = transform.forward;
+    
+        Debug.DrawRay(transform.position, throwDirection * 5f, Color.blue, 2f);
+        Vector3 upwardVector = Vector3.up * 1f; // Use world up direction instead of transform.up
+        Vector3 forceVector = (throwDirection + upwardVector).normalized * 100f;
+        ice.GetComponent<Rigidbody>().AddForce(forceVector, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/Weapons/Bullets/Ice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+using UnityEngine;
+public class Ice : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public void Awake()
+    {
+        SoundManager.PlaySound(SoundManager.Sound.Grenade);
+    }
+    
+    public  void OnCollisionEnter(Collision other)
+    {
+        //check if bullet hits environment or player
+        if (other.gameObject.layer == 6 || other.gameObject.CompareTag("Player"))
+        {
+            ShakeScreen.Instance.ShakeCamera(5f,0.1f);
+            Player.Instance.DecreaseHealth(10f);
+            Instantiate(hitEffect, transform.position, quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
